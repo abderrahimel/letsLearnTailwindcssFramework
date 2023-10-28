3e3f8c417d4ca4dffcba63cd6e0a55538dc4f1e6
abderrahimel <abderrahimelayadi7@gmail.com>
2023-10-29 01:52:57 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //  means look for all those file .html
+    content: ['./build/*.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
